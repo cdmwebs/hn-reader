a3d64d3a3bfb0f6a9530f87d6dd1445e314b33df
Chris Moore <chris@cdmwebs.com>
2016-03-19 22:52:34 -0400

    Add keyboard shortcuts

--- app/routes/items.js
+++ app/routes/items.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
+import KeyboardShortcuts from 'ember-keyboard-shortcuts/mixins/route';
 
-export default Ember.Route.extend({
+const $ = Ember.$;
+
+export default Ember.Route.extend(KeyboardShortcuts, {
   model() {
     return this.store.findAll('item');
   },
@@ -9,6 +12,34 @@ export default Ember.Route.extend({
     items.forEach(function(item) {
       item.reload();
     });
+  },
+
+  keyboardShortcuts: {
+    'j': {
+      action: 'nextItem',
+      global: false
+    },
+    'k': {
+      action: 'previousItem',
+      global: false
+    }
+  },
+
+  actions: {
+    nextItem() {
+      let currentItem = $('.ember-view .active');
+      if (currentItem.next().length === 0) { return; }
+      let nextItem = currentItem.next().attr('href').split('/')[2];
+      this.transitionTo('items.item', nextItem);
+    },
+
+    previousItem() {
+      let currentItem = $('.ember-view .active');
+      if (currentItem.prev().length === 0) { return; }
+      let previousItem = currentItem.prev().attr('href').split('/')[2];
+      this.transitionTo('items.item', previousItem);
+    }
   }
+
 });
 
