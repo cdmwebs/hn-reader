d8a63cfc63d8f39c603488803053839715a4a366
Chris Moore <chris@cdmwebs.com>
2016-03-20 21:13:38 -0400

    Log changes to score/comment count

--- app/components/item-list-item.js
+++ app/components/item-list-item.js
@@ -6,10 +6,12 @@ export default Ember.Component.extend({
   classNames: 'item',
 
   scoreDidChange: observer('item.score', function() {
+    console.log('score changed: ' + this.get('item.id') + ' ' + this.get('item.title'));
     this.$('.item__score').toggleClass('changed');
   }),
 
   commentCountDidChange: observer('item.kids.length', function() {
+    console.log('comments added: ' + this.get('item.id') + ' ' + this.get('item.title'));
     this.$('.item__comment__count').toggleClass('changed');
   })
 });
